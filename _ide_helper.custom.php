798cced485d2aab04da591bb0c5f9d7adb84177c
Willem Franco <willemffrancoc@gmail.com>
2026-03-30 16:18:19 -0500

    Installing ide-helper - Adding custom helper

---
+++ _ide_helper.custom.php
@@ -0,0 +1,16 @@
+<?php
+
+/* @noinspection ALL */
+// @formatter:off
+// phpcs:ignoreFile
+
+/**
+ * A helper file for Laravel, to provide autocomplete information to your IDE
+ */
+
+namespace Illuminate\Http {
+    /**
+     * @method \App\Models\User user($guard = null)
+     */
+    class Request {}
+}
